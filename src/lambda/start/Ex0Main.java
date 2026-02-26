ac97beb0ff1f6421ad56ae7126eaf44056af2138
DESKTOP-LB90PMN\kyw <rladyddn258@gmail.com>
2026-02-26 21:10:12 +0900

    5강. 람다가 필요한이유 -1 [다시한번더]

---
+++ src/lambda/start/Ex0Main.java
@@ -0,0 +1,31 @@
+package lambda.start;
+
+public class Ex0Main {
+
+    public static void helloJava(){
+        System.out.println("프로그램 시작");
+        System.out.println("Hello Java");
+        System.out.println("프로그램 종료");
+    }
+
+    public static void helloSpring(){
+        System.out.println("프로그램 시작");
+        System.out.println("Hello Spring");
+        System.out.println("프로그램 종료");
+    }
+
+    public static void hello(String message){
+        System.out.println("프로그램 시작");
+        System.out.println(message);
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+//        helloJava();
+//        helloSpring();
+        hello("Hello Java");
+        hello("Hello Spring");
+    }
+
+
+}
